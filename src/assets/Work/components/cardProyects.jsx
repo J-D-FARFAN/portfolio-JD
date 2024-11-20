ffee1ef823ae9d59f611394b34e4432dfab1e76a
J-D-FARFAN <juan.frontendMJ@gmail.com>
2024-11-20 16:25:24 -0500

    update styles and implementation data

--- src/assets/Work/components/cardProyects.jsx
+++ src/assets/Work/components/cardProyects.jsx
@@ -1,40 +1,51 @@
 import { ArrowUpRight } from "lucide-react";
-import imagePrueba from "../../../../public/images/GraficodePrecios.png";
 import "./cardProyects.css";
 
-export function CardProyectsPortfolio() {
+export function CardProyectsPortfolio({
+  titleProyectCard,
+  skills,
+  description,
+  date,
+  imageProyect,
+  URL,
+}) {
   return (
     <>
       <section className="proyectsCard">
         <div className="bx-imageProyect">
-          <img src={imagePrueba} alt="" className="image-proyectsCard" />
+          <img src={imageProyect} alt="" className="image-proyectsCard" />
         </div>
 
         <div className="bx-allInformation-proyectsCard">
-          <h3 className="titleProyectCard">Grafico de Gastos</h3>
+          <h3 className="titleProyectCard">{titleProyectCard}</h3>
           <section className="bx-tecnologyUsed-and-state">
             <div className="bx-tecnologyUsed">
-              <span className="stylesGeneralTecnologyUsed html">HTML</span>
-              <span className="stylesGeneralTecnologyUsed css">CSS</span>
-              <span className="stylesGeneralTecnologyUsed react">REACT</span>
+              <span className="stylesGeneralTecnologyUsed html">
+                {skills[0]}
+              </span>
+              <span className="stylesGeneralTecnologyUsed css">
+                {skills[1]}
+              </span>
+              <span className="stylesGeneralTecnologyUsed react">
+                {skills[2]}
+              </span>
             </div>
 
             <div className="bx-state"></div>
           </section>
 
           <div className="proyectsCard-description">
-            <p className="txt">
-              En este desafío, creé un gráfico de barras desde cero y utilicé un
-              archivo JSON local para agregar los datos de forma dinámica.
-            </p>
+            <p className="txt">{description}</p>
           </div>
         </div>
 
         <div className="proyectsCard-month-year-and-seeProyect">
-          <span className="month-year">Octubre | 2024</span>
+          <span className="month-year">{date}</span>
 
           <span className="bx-iconArrow">
-            <ArrowUpRight />
+            <a className="iconArrow" href={URL} target="__blank">
+              <ArrowUpRight color="#121212" />
+            </a>
           </span>
         </div>
       </section>
--- src/assets/Work/proyect.jsx
+++ src/assets/Work/proyect.jsx
@@ -1,8 +1,16 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, CircleX } from "lucide-react";
 import { CardProyectsPortfolio } from "./components/cardProyects";
 import "./proyect.css";
+import dataProyects from "/src/data/proyects.json";
+import { useState } from "react";
 
 export function ProyectsPortfolio() {
+  const [showMore, setShowMore] = useState(false);
+
+  const ShowMoreProyects = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <>
       <article className="proyectPortfolio-contentAll">
@@ -17,18 +25,37 @@ export function ProyectsPortfolio() {
           <span className="titleJobs">Ultimos Trabajos</span>
 
           <figure className="bx-proyectsCards">
-            <CardProyectsPortfolio />
-            <CardProyectsPortfolio />
-            <CardProyectsPortfolio />
+            {dataProyects.slice(0, 3).map((elements) => {
+              return (
+                <>
+                  <CardProyectsPortfolio
+                    titleProyectCard={elements.projectTitle}
+                    skills={elements.technologies}
+                    description={elements.description}
+                    date={elements.date}
+                    imageProyect={elements.image}
+                    URL={elements.URL}
+                  />
+                  ;
+                </>
+              );
+            })}
           </figure>
 
-          <section className="bx-moreProyects">
+          <section className="bx-moreProyects" onClick={ShowMoreProyects}>
             <div className="moreProyects">Ver más</div>
             <div className="bx-iconArrowRight">
               <ChevronRight size={15} />
             </div>
           </section>
         </section>
+        {showMore && (
+          <article className="content__modalState">
+            <span className="closeModal" onClick={ShowMoreProyects}>
+              <CircleX />
+            </span>
+          </article>
+        )}
       </article>
     </>
   );
