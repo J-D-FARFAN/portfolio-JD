1f49d15ca41a4cdaaad8f81084cbadc27df48674
J-D-FARFAN <juan.frontendMJ@gmail.com>
2024-11-25 13:34:51 -0500

    update component proyects and data

--- src/assets/Work/proyect.jsx
+++ src/assets/Work/proyect.jsx
@@ -6,9 +6,18 @@ import { useState } from "react";
 
 export function ProyectsPortfolio() {
   const [showMore, setShowMore] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
 
   const ShowMoreProyects = () => {
-    setShowMore(!showMore);
+    if (showMore) {
+      setIsClosing(true); // Activa la animación de cierre
+      setTimeout(() => {
+        setShowMore(false); // Cierra el modal después de la animación
+        setIsClosing(false); // Restablece el estado de cierre
+      }, 500);
+    } else {
+      setShowMore(true);
+    }
   };
 
   return (
@@ -22,38 +31,60 @@ export function ProyectsPortfolio() {
         </div>
 
         <section className="proyectPortfolio-content-proyectsCards">
-          <span className="titleJobs">Ultimos Trabajos</span>
+          <span className="titleJobs">Últimos Trabajos</span>
 
           <figure className="bx-proyectsCards">
             {dataProyects.slice(0, 3).map((elements) => {
               return (
-                <>
-                  <CardProyectsPortfolio
-                    titleProyectCard={elements.projectTitle}
-                    skills={elements.technologies}
-                    description={elements.description}
-                    date={elements.date}
-                    imageProyect={elements.image}
-                    URL={elements.URL}
-                  />
-                  ;
-                </>
+                <CardProyectsPortfolio
+                  key={elements.ID}
+                  titleProyectCard={elements.projectTitle}
+                  skills={elements.technologies}
+                  description={elements.description}
+                  date={elements.date}
+                  imageProyect={elements.image}
+                  URL={elements.URL}
+                />
               );
             })}
           </figure>
 
           <section className="bx-moreProyects" onClick={ShowMoreProyects}>
-            <div className="moreProyects">Ver más</div>
+            <div className="moreProyects">Ver todos</div>
             <div className="bx-iconArrowRight">
               <ChevronRight size={15} />
             </div>
           </section>
         </section>
         {showMore && (
-          <article className="content__modalState">
+          <article
+            className={`content__modalState ${
+              isClosing ? "modal--hidden" : "modal--visible"
+            }`}
+          >
             <span className="closeModal" onClick={ShowMoreProyects}>
-              <CircleX />
+              <CircleX color="var(--colorFT-white)" />
             </span>
+
+            <section className="bx-contentALL-proyectsPortfolio">
+              <span className="titleJobs">Todos mis proyectos</span>
+
+              <figure className="bx-proyectsCards">
+                {dataProyects.map((elements) => {
+                  return (
+                    <CardProyectsPortfolio
+                      key={elements.ID}
+                      titleProyectCard={elements.projectTitle}
+                      skills={elements.technologies}
+                      description={elements.description}
+                      date={elements.date}
+                      imageProyect={elements.image}
+                      URL={elements.URL}
+                    />
+                  );
+                })}
+              </figure>
+            </section>
           </article>
         )}
       </article>
